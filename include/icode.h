13012d23b9746765a77d367319467d10f3d35c73
Essien Ita Essien <essiene@gmail.com>
2009-12-19 12:04:25 +0100

    Implement intermediate code ADT - ICode
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

---
+++ include/icode.h
@@ -0,0 +1,40 @@
+#ifndef _ICODE_H
+#define _ICODE_H
+
+#include <gc.h>
+#include <ast.h>
+
+#define  ICODE_NEW(ic, type) ICode ic = GC_malloc(sizeof(*ic)); \
+                             if(ic == NULL) { \
+                                 return NULL; \
+                             } \
+                             ic->type = type; \
+                             ic->tail = NULL; \
+                             ic->next = NULL; \
+
+typedef struct _ICode* ICode;
+
+extern ICode iCode;
+
+typedef enum {
+    I_ASSIGN_STRING,
+    I_ASSIGN_NUMBER,
+    I_ASSIGN_BOOLEAN,
+    I_ASSIGN_IDENTIFIER,
+    I_ASSIGN_BINOP
+} ICodeType;
+
+
+ICode icode_new();
+ICode icode_assign_new(ICodeType);
+ICode icode_append(ICode,ICode);
+ICode icode_name_set(ICode, const char *);
+ICode icode_number_set(ICode, int);
+ICode icode_string_set(ICode, const char*);
+ICode icode_boolean_set(ICode, LyraBoolean);
+ICode icode_identifier_set(ICode, const char*);
+ICode icode_binop_set(ICode, int, const char*, const char*);
+
+void icode_printf(ICode, int);
+
+#endif
---
+++ src/icode.c
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <lyra.h>
+
+ICode iCode;
+
+struct _ICode {
+    ICodeType type;
+    char* name;
+    union {
+        char* identifier;
+        char* string;
+        int number;
+        LyraBoolean boolean;
+
+        struct {
+            char* left;
+            int op;
+            char* right;
+        } binop;
+    } value;
+
+    ICode next;
+    ICode tail;
+};
+
+
+ICode icode_new()
+{
+    return NULL;
+}
+
+ICode icode_assign_new(ICodeType type)
+{
+    ICODE_NEW(ic, type);
+    return ic;
+}
+
+ICode icode_append(ICode head, ICode new)
+{
+    if(head == NULL) {
+        return new;
+    }
+
+    if(new == NULL) {
+        return head;
+    }
+
+    if(head->next == NULL) {
+        head->next = new;
+        head->tail = new;
+        return head;
+    }
+
+    head->tail->next = new;
+
+    if(new->tail == NULL) {
+        head->tail = new;
+    } else {
+        head->tail = new->tail;
+    }
+
+    return head;
+}
+
+ICode icode_name_set(ICode ic, const char* name)
+{
+    ic->name = strndup(name, 255);
+    return ic;
+}
+
+ICode icode_number_set(ICode ic, int i)
+{
+    ic->value.number = i;
+    return ic;
+}
+
+ICode icode_string_set(ICode ic, const char* s)
+{
+    ic->value.string = strndup(s, 255);
+    return ic;
+}
+
+ICode icode_boolean_set(ICode ic, LyraBoolean b)
+{
+    ic->value.boolean = b;
+    return ic;
+}
+
+ICode icode_identifier_set(ICode ic, const char *id)
+{
+    ic->value.identifier = strndup(id, 255);
+    return ic;
+}
+
+ICode icode_binop_set(ICode ic, int op, const char* left, const char* right)
+{
+    ic->value.binop.left = strndup(left, 255);
+    ic->value.binop.op = op;
+    ic->value.binop.right = strndup(right, 255);
+    return ic;
+}
+
+void icode_printf(ICode ic, int i)
+{
+    if(ic == NULL) {
+        printf("EOF\n");
+        return;
+    }
+
+    printf("%d: ", i);
+
+    switch(ic->type) {
+        case I_ASSIGN_STRING: printf("%s = %s\n", ic->name, ic->value.string);break;
+        case I_ASSIGN_NUMBER: printf("%s = %d\n", ic->name, ic->value.number);break;
+        case I_ASSIGN_BOOLEAN: printf("%s = %d\n", ic->name, ic->value.boolean);break;
+        case I_ASSIGN_IDENTIFIER: printf("%s = %s\n", ic->name, ic->value.identifier);break;
+        case I_ASSIGN_BINOP: printf("%s = %s %c %s\n", ic->name,
+                                     ic->value.binop.left,
+                                     ic->value.binop.op,
+                                     ic->value.binop.right);break;
+        default: fprintf(stderr, "Unknown Intermediate Code type\n");return;
+    }
+
+    i += 1;
+    icode_printf(ic->next, i);
+}
